ecef5e76440668a408c342d52476dc1b93af1a95
unknown <xuzhibin28@gmail.com>
2026-04-21 14:24:17 +0800

    assignment

---
+++ task2/db.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "localhost"; 
+$username = "root";        
+$password = "";            
+$dbname = "quetion1";      
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die( "db base error" . $conn->connect_error);
+} 
+?>
